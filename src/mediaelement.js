357846242a127c2f628acd3a7fed602c5954f2a0
paulyc <paulyc@quetzalcoatl.us>
2021-03-24 09:56:19 -0400

    dont set time to a non finite number

--- src/mediaelement.js
+++ src/mediaelement.js
@@ -275,7 +275,7 @@ export default class MediaElement extends WebAudio {
      * @param {number} start Position to start at in seconds
      */
     seekTo(start) {
-        if (start != null) {
+        if (Number.isFinite(start)) {
             this.media.currentTime = start;
         }
         this.clearPlayEnd();
